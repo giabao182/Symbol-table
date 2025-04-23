1886ded9d52e7245c2cb3ca32dd71e2154d78f78
giabao182 <gbao1822005@gmail.com>
2025-04-24 02:13:42 +0700

    2:13 24/4/2025

--- SymbolTable.py
+++ SymbolTable.py
@@ -27,18 +27,7 @@ def getIndex(scopes, name) -> int:
     else:
         return getIndex(scopes[:-1],name)
 
-def valid_assign_with_oldVariable(type, name,scopes):
-    if not scopes:
-        return False
-    if name in scopes[-1]:
-        if type == scopes[-1][name]["type"]:
-            return True
-        else:
-            return False
-    else:
-        return valid_assign_with_oldVariable(type,name,scopes[:-1])
-
-def assign_all_scopes(scopes, name, value,originscopes) -> int:
+def check_assign(scopes, name, value,originscopes) -> int:
     if not scopes:
         return 0 #Undeclare
     current_scope = scopes[-1]
@@ -72,43 +61,22 @@ def assign_all_scopes(scopes, name, value,originscopes) -> int:
                 current_scope[name]["value"] = value 
                 return 1 #typeMis
         return 2
-    return assign_all_scopes(scopes[:-1], name, value,originscopes)
-
-def check_redeclare(scopes, name):
-    if name in scopes[-1]:
-        return True
-    return False
+    return check_assign(scopes[:-1], name, value,originscopes)
 
 def is_valid_identifier(name: str) -> bool:
     return (name and name[0].islower() and all(c.isalnum() or c == '_' for c in name))
 
-def is_valid_value(value: str) -> bool:
-    if value[0] == "'" and value[-1] == "'":
-        if not all(c.isalnum() or c == ' ' for c in value[1:-1]):
-            return 0
-    else:
-        if not value.isdigit():
-            return 0
-    return 1
-    
-def count_spaces(s: str) -> int:
-    return s.count(' ')
-
-def check_insert_format(command: str) -> bool:
-    parts = command.strip().split()
-    return (len(parts) == 3 and parts[0] == "INSERT" and is_valid_identifier(parts[1]) and parts[2] in ["number", "string"])
-
 def process_command(command, symbols, scopes):
     parts = command.strip().split()
-    numspaces = count_spaces(command)
+    numspaces = command.count(' ')
 
     if not parts or numspaces != len(parts) - 1:
         raise InvalidInstruction(command)
 
     if parts[0] == "INSERT":
-        if not check_insert_format(command):
+        if not (len(parts) == 3 and parts[0] == "INSERT" and is_valid_identifier(parts[1]) and parts[2] in ["number", "string"]):
             raise InvalidInstruction(command)
-        if check_redeclare(scopes, parts[1]):
+        if parts[1] in scopes[-1]:
             raise Redeclared(command)
         symbols.append(Symbol(parts[1], parts[2]))
         scopes[-1][parts[1]] = {"type": parts[2], "value": None}
@@ -119,7 +87,7 @@ def process_command(command, symbols, scopes):
             raise InvalidInstruction(command)
         name = parts[1]
         value = parts[2]
-        result = assign_all_scopes(scopes, name, value,scopes)
+        result = check_assign(scopes, name, value,scopes)
         if result == 0:
             raise Undeclared(command)
         elif result == 1:
@@ -170,18 +138,6 @@ def process_command(command, symbols, scopes):
     else: 
         raise InvalidInstruction(command)
 
-# def process_commands(commands, result_list, symbols, scopes):
-#     if not commands:
-#         if len(scopes) > 1:
-#             return process_commands(commands, result_list + [str(UnclosedBlock((len(scopes)-1)))], symbols, [{}])
-#         return result_list
-#     try:
-#         result, new_symbols, new_scopes = process_command(commands[0], symbols, scopes)
-#         if result is None:
-#             return process_commands(commands[1:], result_list, new_symbols, new_scopes)
-#         return process_commands(commands[1:], result_list + [result], new_symbols, new_scopes)
-#     except StaticError as e:
-#         return process_commands(commands[1:], result_list + [str(e)], symbols, scopes)
 def process_commands(commands, result_list, symbols, scopes):
     if not commands:
         if len(scopes) > 1:
@@ -198,4 +154,3 @@ def process_commands(commands, result_list, symbols, scopes):
 
 def simulate(list_of_commands):
     return process_commands(list_of_commands, [], [], [{}])
-print(simulate(["INSERT x string", "INSERT y string", "BEGIN", "INSERT y number", "ASSIGN x y", "END"]))
